f4017c6bab44d8f75d32f39b7a4056943bb3427b
xXAbieGamingXx <65514695+xXAbieGamingXx@users.noreply.github.com>
2026-06-09 22:48:05 -0700

    yes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-sudo apt install python3 python3-venv python3-pip neovim
+sudo apt -y install python3 python3-venv python3-pip neovim
 sudo mv iol.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable iol.service
